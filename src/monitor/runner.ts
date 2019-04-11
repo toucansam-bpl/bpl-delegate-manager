8b87e1b3cd9f61ba976d96bad05fd19ae50ebce0
toucansam <gumbyclownshoes@gmail.com>
2019-04-11 15:18:31 -0500

    Changed monitor default command to monitor:run

--- src/monitor/runner.ts
+++ src/monitor/runner.ts
@@ -5,7 +5,7 @@ const fifteenMinutes = 15 * 60 * 1000
 console.log(`Starting BPL node monitor...`)
 
 async function checkNodeStatus() {
-  const { stdout, stderr } = await exec('bpl-dms monitor')
+  const { stdout, stderr } = await exec('bpl-dms monitor run')
   console.log('stdout:', stdout)
   console.log('stderr:', stderr)
 }
