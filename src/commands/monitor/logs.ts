b2f81f8e00542afcf01125d783c8de0f7c3486e9
toucansam <gumbyclownshoes@gmail.com>
2019-04-11 15:45:09 -0500

    Added lines flag to log commands

--- src/commands/monitor/logs.ts
+++ src/commands/monitor/logs.ts
@@ -1,9 +1,18 @@
 import { Command } from '@oclif/command'
+import { flags } from '@oclif/parser'
 import { spawn } from 'child_process'
 
 
 export class MonitorLogsCommand extends Command {
+  static flags = {
+    lines: flags.integer(),
+  }
+
   async run() {
-    spawn('npx', ['pm2', 'logs', 'bpl-monitor'], { stdio: 'inherit' })
+    const { flags } = this.parse(MonitorLogsCommand)
+    const commandArgs = ['pm2', 'logs', 'bpl-monitor']
+    const flagArgs = flags.lines ? ['--lines', flags.lines.toString()] : []
+
+    spawn('npx', commandArgs.concat(flagArgs), { stdio: 'inherit' })
   }
 }
--- src/commands/node/logs.ts
+++ src/commands/node/logs.ts
@@ -1,9 +1,18 @@
 import { Command } from '@oclif/command'
+import { flags } from '@oclif/parser'
 import { spawn } from 'child_process'
 
 
 export class NodeLogsCommand extends Command {
+  static flags = {
+    lines: flags.integer(),
+  }
+
   async run() {
-    spawn('npx', ['pm2', 'logs', 'bpl-node'], { stdio: 'inherit' })
+    const { flags } = this.parse(NodeLogsCommand)
+    const commandArgs = ['pm2', 'logs', 'bpl-node']
+    const flagArgs = flags.lines ? ['--lines', flags.lines.toString()] : []
+
+    spawn('npx', commandArgs.concat(flagArgs), { stdio: 'inherit' })
   }
 }
